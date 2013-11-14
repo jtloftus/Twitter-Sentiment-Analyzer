12176c71cde248ba86de0166b767bd9b56fe3f6b
Joe Loftus <jtloftus@gmail.com>
2013-11-14 17:27:40 -0500

    Proper Layout

--- Twitter Sentiment Analyzer/SAViewController.m
+++ Twitter Sentiment Analyzer/SAViewController.m
@@ -10,6 +10,12 @@
 
 @interface SAViewController ()
 
+@property (strong, nonatomic) IBOutlet UILabel *handleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *approvalLabel;
+@property (strong, nonatomic) IBOutlet UITextField *queryTextField;
+@property (strong, nonatomic) IBOutlet UITableView *tweetTableView;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
 @end
 
 @implementation SAViewController
@@ -18,12 +24,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.activityIndicator.hidesWhenStopped = YES;
+    self.approvalLabel.hidden = YES;
+    
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)signIn:(id)sender {
 }
+
+- (IBAction)queryTweets:(id)sender {
+}
+
+
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
 
 @end
